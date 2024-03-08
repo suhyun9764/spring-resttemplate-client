f2f5d88c4868816fe003b4ee4047e552cfe76036
suhyun9764 <suhyun9764@naver.com>
2024-03-08 16:59:50 +0900

    feat(): RestTemplate get list 요청 구현

--- src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
+++ src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
@@ -2,10 +2,16 @@ package com.sparta.springresttemplateclient.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.json.JSONObject;
 
 @Getter
 @NoArgsConstructor
 public class ItemDto {
     private String title;
     private int price;
+
+    public ItemDto(JSONObject item) {
+        this.title = item.getString("title");
+        this.price = item.getInt("price");
+    }
 }
